d6b1d5459322c1c37658300c777c6d8183a78af6
spacewander <spacewanderlzx@gmail.com>
2014-12-27 23:09:47 +0800

    Test before development

---
+++ c++/main.cpp
@@ -0,0 +1,9 @@
+ #include "gtest.h"
+
+int main(int argc, char** argv) 
+{ 
+    testing::InitGoogleTest(&argc, argv); 
+
+    // Runs all tests using Google Test. 
+    return RUN_ALL_TESTS(); 
+ }
---
+++ c++/test.cpp
@@ -0,0 +1,6 @@
+ #include "gtest.h"
+
+ TEST(AlgorithmTest, testWork)
+ { 
+     ASSERT_EQ(2, 1 + 1);
+ }
