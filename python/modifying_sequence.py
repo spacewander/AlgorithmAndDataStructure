b44f9691332ee57b2075d623aa7dc917c8c4e1cd
spacewander <spacewanderlzx@gmail.com>
2015-01-01 20:41:22 +0800

    python : add more modifying sequence algorithms

--- python/modifying_sequence.py
+++ python/modifying_sequence.py
@@ -1,5 +1,5 @@
 # Implementations of modifying sequence algorithms
-from itertools import islice, ifilter
+from itertools import islice, ifilter, ifilterfalse, imap
 
 def copy(iter, start, end):
     return islice(iter, start, end)
@@ -29,9 +29,8 @@ def fill_n(seq, value, count):
     for i in range(0, count) :
         seq[i] = value
 
-def transform(iter1, iter2, func):
-    iter2.__init__([func(i) for i in iter1])
-    return iter2
+def transform(iter, func):
+    return [func(i) for i in iter]
 
 def generate(seq, func):
     for i, _ in enumerate(seq) :
@@ -40,3 +39,34 @@ def generate(seq, func):
 def generate_n(seq, func, count):
     for i in range(0, count) :
         seq[i] = func()
+
+# the problem is, iterate in dict return (k1, k2, ...) instead of ((k1, v1), ...)
+def remove(iter, value):
+    return ifilterfalse(lambda x: x == value, iter)
+
+def remove_if(iter, func):
+    return ifilterfalse(func, iter)
+
+def remove_copy(iter, value):
+    return [i for i in remove(iter, value)]
+
+def remove_copy_if(iter, func):
+    return [i for i in remove_if(iter, func)]
+
+def replace(iter, before, after):
+    def _replace_func(x):
+        if x == before : return after
+        else : return x
+    return imap(_replace_func, iter)
+
+def replace_if(iter, func, after):
+    def _replace_if(x):
+        if func(x) : return after
+        else : return x
+    return imap(_replace_if, iter)
+
+def replace_copy(iter, before, after):
+    return [i for i in replace(iter, before, after)]
+
+def replace_copy_if(iter, func, after):
+    return [i for i in replace_if(iter, func, after)]
--- python/test_modifying_sequence.py
+++ python/test_modifying_sequence.py
@@ -5,6 +5,14 @@ from modifying_sequence import *
 def ary():
     return [1, 2, 3, 4, 5]
 
+@pytest.fixture
+def set_x():
+    return set([1, 2, 3])
+
+@pytest.fixture
+def dict_x():
+    return dict([(1, '1'), (2, '2')])
+
 def test_copy(ary):
     assert [i for i in copy(ary, 0, 3)] == ary[:3]
 
@@ -31,10 +39,11 @@ def test_fill_n(ary):
     fill_n(ary, 3, 2)
     assert ary == [3, 3, 3, 4, 5]
 
-def test_transform(ary):
-    assert transform(ary, [], lambda x: x + 1) == [2, 3, 4, 5, 6]
-    a = dict([(1, '1'), (2, '2')])
-    assert transform(a, {}, lambda k: (k, k)) == dict([(1, 1), (2, 2)])
+def test_transform(ary, dict_x):
+    a = transform(ary, lambda x: x + 1)
+    assert a == [2, 3, 4, 5, 6]
+    b = dict(transform(dict_x, lambda k: (k, k)))
+    assert b == dict([(1, 1), (2, 2)])
 
 def test_generate(ary):
     generate(ary, lambda :3)
@@ -43,3 +52,40 @@ def test_generate(ary):
 def test_generate_n(ary):
     generate_n(ary, lambda :3, 3)
     assert ary == [3, 3, 3, 4, 5]
+
+def test_remove(ary, set_x):
+    assert [i for i in remove(ary, 4)] == [1, 2, 3, 5]
+    assert set([i for i in remove(set_x, 3)]) == set([1, 2])
+    # can't work on dict
+    # assert [i for i in remove(dict_x, 1)] == [(2, '2')]
+
+def test_remove_if(ary):
+    assert [i for i in remove_if(ary, lambda x: x < 3)] == [3, 4, 5]
+
+def test_remove_copy(ary, set_x):
+    a = remove_copy(ary, 4)
+    assert a == [1, 2, 3, 5]
+    b = set(remove_copy(set_x, 3))
+    assert b == set([1, 2])
+
+def test_remove_copy_if(ary):
+    a = remove_copy_if(ary, lambda x: x < 3)
+    assert a == [3, 4, 5]
+
+def test_replace(ary, set_x):
+    assert [i for i in replace(ary, 4, 5)] == [1, 2, 3, 5, 5]
+    assert set([i for i in replace(set_x, 3, 4)]) == set([1, 2, 4])
+
+def test_replace_if(ary):
+    assert [i for i in replace_if(ary, lambda x: x < 3, 1)] == \
+            [1, 1, 3, 4, 5]
+
+def test_replace_copy(ary, set_x):
+    a = replace_copy(ary, 4, 3)
+    assert a == [1, 2, 3, 3, 5]
+    b = set(replace_copy(set_x, 3, 4))
+    assert b == set([1, 2, 4])
+
+def test_replace_copy_if(ary):
+    a = replace_copy_if(ary, lambda x: x < 3, 1)
+    assert a == [1, 1, 3, 4, 5]
