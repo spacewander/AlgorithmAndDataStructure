fb44a01a27c277b8c204380572c3c792483dfe69
spacewander <spacewanderlzx@gmail.com>
2015-01-17 20:32:23 +0800

    javascript : add more modifying sequence algorithms

--- javascript/modifying_sequence.js
+++ javascript/modifying_sequence.js
@@ -1,13 +1,15 @@
-var copy = function(coll, dest) {
-  for (var i in coll)
+var NonModifyingSequence = require('./non_modifying_sequence');
+
+var copy = function(src, dest) {
+  for (var i in src)
     if (dest[i] === undefined) 
-      dest[i] = coll[i];
+      dest[i] = src[i];
 };
 
-var copy_if = function(coll, dest, cb) {
-  for (var i in coll)
-    if (dest[i] === undefined && cb(coll[i]))
-      dest[i] = coll[i];
+var copy_if = function(src, dest, cb) {
+  for (var i in src)
+    if (dest[i] === undefined && cb(src[i]))
+      dest[i] = src[i];
 };
 
 var copy_n = function(dest, n, value) {
@@ -24,9 +26,79 @@ var copy_backward = function(src, dest) {
   return src.length - dest.length >= 0 ? src.length - dest.length : -1;
 };
 
+var move = function(src, dest) {
+  for (var i in src)
+    if (dest[i] === undefined) 
+      dest[i] = src[i];
+};
+
+var move_backward = function(src, dest) {
+  var j = 0;
+  for (var i = src.length - dest.length; i >= 0 && i < src.length; i++, j++) {
+    src[i] = dest[j];
+  }
+  return src.length - dest.length >= 0 ? src.length - dest.length : -1;
+};
+
+var fill = function(ary, value) {
+  for (var i = 0; i < ary.length; i++)
+    ary[i] = value;
+};
+
+var fill_n = function(ary, n, value) {
+  for (var i = 0; i < n; i++)
+    ary[i] = value;
+};
+
+var transform = function(coll, cb) {
+  NonModifyingSequence.for_each(coll, cb);
+  return coll;
+};
+
+var generate = function(ary, g) {
+  for (var i = 0; i < ary.length; i++)
+    ary[i] = g();
+};
+
+var generate_n = function(ary, n, g) {
+  for (var i = 0; i < n; i++)
+    ary[i] = g();
+};
+
+var remove_if = function(coll, cb) {
+  var i;
+  if (coll.hasOwnProperty('length')) {
+    var copy = coll.constructor();
+    var j = 0;
+    for (i = 0; i < coll.length; i++) {
+      if (!cb(coll[i]))
+        copy[j++] = coll[i];
+    }
+    copy.length = j;
+    while (i-- > j)
+      Array.prototype.shift.call(coll);
+    for (i = 0; i < j; i++)
+      coll[i] = copy[i];
+  }
+  else {
+    for (i in coll) {
+      if (coll.hasOwnProperty(i) && cb(coll[i]))
+        delete coll[i];
+    }
+  }
+};
+
 module.exports = {
   copy: copy,
   copy_if: copy_if,
   copy_n: copy_n,
-  copy_backward: copy_backward
+  copy_backward: copy_backward,
+  move: move,
+  move_backward: move_backward,
+  fill: fill,
+  fill_n: fill_n,
+  transform: transform,
+  generate: generate,
+  generate_n: generate_n,
+  remove_if: remove_if
 };
--- javascript/modifying_sequence_spec.js
+++ javascript/modifying_sequence_spec.js
@@ -39,4 +39,67 @@ describe('Modifying sequence algorithms', function(){
     JSON.stringify(ary).should.be.equal(JSON.stringify([1, 2, 3, 4, 5]));
   });
 
+  // move* is implemented the same as copy*
+  it('move', function(){
+    var hash = {};
+    Algos.move(Hash(), hash);
+    JSON.stringify(hash).should.be.equal(JSON.stringify({'a': 1, 'b': 2}));
+  });
+
+  it('move_backward', function(){
+    var ary = new Array(3);
+    Algos.move_backward(ary, Ary()).should.be.equal(-1);
+    ary = new Array(5);
+    Algos.move_backward(ary, Ary()).should.be.equal(0);
+    JSON.stringify(ary).should.be.equal(JSON.stringify([1, 2, 3, 4, 5]));
+  });
+
+  it('fill', function(){
+    var ary = new Array(3);
+    Algos.fill(ary, 3);
+    JSON.stringify(ary).should.be.equal(JSON.stringify([3, 3, 3]));
+  });
+
+  it('fill_n', function(){
+    var ary = new Array(3);
+    Algos.fill_n(ary, 2, 3);
+    JSON.stringify(ary).should.be.equal(JSON.stringify([3, 3, null]));
+  });
+
+  it('transform', function(){
+    var emptyAry = [];
+    JSON.stringify(Algos.transform(Ary(), function(e){
+      emptyAry.push(e + 1);
+    })).should.be.equal(JSON.stringify(Ary()));
+    JSON.stringify(emptyAry).should.be.equal(JSON.stringify([2, 3, 4, 5, 6]));
+  });
+
+  it('generate', function(){
+    var ary = new Array(3);
+    Algos.generate(ary, function(){
+      return 3;
+    });
+    JSON.stringify(ary).should.be.equal(JSON.stringify([3, 3, 3]));
+  });
+
+  it('generate_n', function(){
+    var ary = new Array(3);
+    Algos.generate_n(ary, 2, function(){
+      return 3;
+    });
+    JSON.stringify(ary).should.be.equal(JSON.stringify([3, 3, null]));
+  });
+
+  it('remove_if', function(){
+    var ary = [1, 2, 3];
+    Algos.remove_if(ary, function(e){
+      return e < 2;
+    });
+    JSON.stringify(ary).should.be.equal(JSON.stringify([2, 3]));
+    var hash = {'a': 1, 'b': 2};
+    Algos.remove_if(hash, function(e){
+      return e != 1;
+    });
+    JSON.stringify(hash).should.be.equal(JSON.stringify({'a': 1}));
+  });
 });
