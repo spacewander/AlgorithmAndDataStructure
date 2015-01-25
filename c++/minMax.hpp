23149dbe725232b4cde5975fdc616eb5bfa4a364
spacewander <spacewanderlzx@gmail.com>
2015-01-25 22:36:18 +0800

    c++ : add other min/max algorithms

--- c++/minMax.hpp
+++ c++/minMax.hpp
@@ -46,7 +46,7 @@ ForwordIt min_element(ForwordIt start, ForwordIt end)
 template<typename T>
 std::pair<const T&, const T&> minmax(const T& a, const T& b)
 {
-    return (a < b) ? std::pair<const T&, const T&>(a, b) 
+    return (a < b) ? std::pair<const T&, const T&>(a, b)
                    : std::pair<const T&, const T&>(b, a);
 }
 
@@ -63,5 +63,81 @@ std::pair<ForwordIt, ForwordIt> minmax_element(ForwordIt start, ForwordIt end)
     }
     return result;
 }
+
+// only work on ascii
+template<typename InputIt1, typename InputIt2>
+bool lexicographical_compare(InputIt1 start1, InputIt1 end1,
+        InputIt2 start2, InputIt2 end2)
+{
+    for (; start1 != end1 && start2 != end2; ++start1, ++start2) {
+        if (*start2 > *start1) return true;
+        if (*start2 < *start1) return false;
+    }
+    return start1 == end1 && start2 != end2;
 }
+
+template<typename ForwordIt1, typename ForwordIt2>
+bool is_permutation(ForwordIt1 start1, ForwordIt1 end1,
+        ForwordIt2 start2, ForwordIt2 end2)
+{
+   if (std::distance(start1, end1) != std::distance(start2, end2))
+       return false;
+   std::tie(start1, start2) = std::mismatch(start1, end1, start2);
+   if (start1 != end1) {
+       for (ForwordIt1 i = start1; i != end1; ++i) {
+           if (i != std::find(start1, i, *i))
+               continue;
+           size_t match = std::count(start2, end2, *i);
+           if (match == 0 || match != std::count(i, end1, *i))
+               return false;
+       }
+   }
+   return true;
+}
+
+template<typename BidirIt>
+bool next_permutation(BidirIt start, BidirIt end)
+{
+    if (start == end || start + 1 == end) return false;
+    BidirIt i = end - 1;
+    while (true) {
+        BidirIt i1 = i, i2;
+        if (*--i < *i1) {
+            i2 = end - 1;
+            while (!(*i < *i2))
+                i2--;
+            std::iter_swap(i, i2);
+            std::reverse(i1, end);
+            return true;
+        }
+        if (i == start) {
+            std::reverse(start, end);
+            return false;
+        }
+    }
+}
+
+template<typename BidirIt>
+bool prev_permutation(BidirIt start, BidirIt end)
+{
+    if (start == end || start + 1 == end) return false;
+    BidirIt i = end - 1;
+    while (true) {
+        BidirIt i1 = i, i2;
+        if (*i1 < *--i) {
+            i2 = end - 1;
+            while (!(*i2 < *i))
+                i2--;
+            std::iter_swap(i, i2);
+            std::reverse(i1, end);
+            return true;
+        }
+        if (i == start) {
+            std::reverse(start, end);
+            return false;
+        }
+    }
+}
+}
+
 #endif /* MINMAX_H */
--- c++/minMaxTest.cpp
+++ c++/minMaxTest.cpp
@@ -1,3 +1,5 @@
+#include <string>
+
 #include "test.h"
 #include "minMax.hpp"
 
@@ -42,4 +44,36 @@ TEST_F(MinMax, minmax_element)
 {
     EXPECT_EQ(1, *my::minmax_element(v.begin(), v.end()).first);
     EXPECT_EQ(5, *my::minmax_element(v.begin(), v.end()).second);
+}
+
+TEST_F(MinMax, lexicographical_compare)
+{
+    string a = "abcd";
+    string b = "abce";
+    EXPECT_TRUE(my::lexicographical_compare(a.begin(), a.end(),
+                b.begin(), b.end()));
+}
+
+TEST_F(MinMax, is_permutation)
+{
+    vector<int> a{1, 2, 3, 4, 5};
+    vector<int> b{1, 2, 3, 4, 4};
+    EXPECT_TRUE(my::is_permutation(v.begin(), v.end(), a.begin(), a.end()));
+    EXPECT_FALSE(my::is_permutation(v.begin(), v.end(), b.begin(), b.end()));
+}
+
+TEST_F(MinMax, next_permutation)
+{
+    vector<int> duplicate(v);
+    EXPECT_TRUE(my::next_permutation(v.begin(), v.end()));
+    std::next_permutation(duplicate.begin(), duplicate.end());
+    EXPECT_EQ(duplicate, v);
+}
+
+TEST_F(MinMax, prev_permutation)
+{
+    vector<int> duplicate(v);
+    EXPECT_TRUE(my::prev_permutation(v.begin(), v.end()));
+    std::prev_permutation(duplicate.begin(), duplicate.end());
+    EXPECT_EQ(duplicate, v);
 }
