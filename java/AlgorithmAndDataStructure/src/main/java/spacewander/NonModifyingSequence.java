b73e7f0f2936551951aa0ae413106ea7e265e385
spacewander <spacewanderlzx@gmail.com>
2015-02-06 20:38:25 +0800

    java's brave new world

---
+++ java/AlgorithmAndDataStructure/src/main/java/spacewander/NonModifyingSequence.java
@@ -0,0 +1,13 @@
+package spacewander;
+
+/**
+ * Hello world!
+ *
+ */
+public class NonModifyingSequence
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
