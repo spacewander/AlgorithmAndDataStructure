080e95843ee33297e9934d65548e3fc50bbafec7
spacewander <spacewanderlzx@gmail.com>
2015-12-05 12:31:55 +0800

    lua : add partition algorithms

---
+++ lua/partition.lua
@@ -0,0 +1,57 @@
+function is_partitioned(seq, func)
+    local pivot = partition_point(seq, func)
+    for i = pivot, #seq do
+        if func(seq[i]) then return false end
+    end
+    return true
+end
+
+function partition_point(seq, func)
+    for i = 1, #seq do
+        if not func(seq[i]) then return i end
+    end
+    return #seq + 1
+end
+
+function partition(seq, func)
+    local pivot = partition_point(seq, func)
+    for i = pivot, #seq do
+        if func(seq[i]) then
+            seq[i], seq[pivot] = seq[pivot], seq[i]
+            pivot = pivot + 1
+        end
+    end
+end
+
+function partition_copy(seq, func)
+    local copy = {}
+    for i = 1, #seq do
+        if func(seq[i]) then
+            table.insert(copy, 1, seq[i])
+        else
+            table.insert(copy, seq[i])
+        end
+    end
+    return copy
+end
+
+function stable_partition(seq, func)
+    local seq_size = #seq
+    local idx = 1
+    for time = 1, seq_size do
+        if not func(seq[idx]) then
+            table.insert(seq, seq[idx])
+            table.remove(seq, idx)
+        else
+            idx = idx + 1
+        end
+    end
+end
+
+return {
+    is_partitioned = is_partitioned,
+    partition_point = partition_point,
+    partition = partition,
+    partition_copy = partition_copy,
+    stable_partition = stable_partition
+}
---
+++ lua/partition_spec.lua
@@ -0,0 +1,38 @@
+local Algos = require('./partition')
+
+function less_than_3(num)
+    return num < 3
+end
+
+describe('partition algorithms', function()
+    setup(function ()
+        seq = {1, 2, 3, 5, 2}
+    end)
+
+    it('is_partitioned', function()
+        assert.is_false(Algos.is_partitioned(seq, less_than_3))
+        assert.is_true(Algos.is_partitioned({2, 1, 2}, less_than_3))
+    end)
+
+    it('partition_point', function()
+        assert.are.same(3, Algos.partition_point(seq, less_than_3))
+        assert.are.same(4, Algos.partition_point({2, 1, 2}, less_than_3))
+    end)
+
+    it('partition', function()
+        local seq = {1, 2, 4, 2}
+        Algos.partition(seq, less_than_3)
+        assert.are.same(4, seq[4])
+    end)
+
+    it('partition_copy', function()
+        local copy = Algos.partition_copy(seq, less_than_3)
+        assert.are.same(3, copy[4])
+    end)
+
+    it('stable_partition', function()
+        local seq = {1, 2, 3, 1, 2, 5, 4}
+        Algos.stable_partition(seq, less_than_3)
+        assert.are.same({1, 2, 1, 2, 3, 5, 4}, seq)
+    end)
+end)
