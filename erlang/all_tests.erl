21dc079e9e424797b1dd3c35b5361a50e25d54df
spacewander <spacewanderlzx@gmail.com>
2017-10-05 14:51:47 +0800

    Remove black history

--- erlang/all_tests.erl
+++
@@ -1,9 +0,0 @@
--module(all_tests).
--export([test/0]).
-
-test() ->
-    lists:foreach(
-        fun(Mod) -> 
-            eunit:test(list_to_atom(string:concat(Mod, "_tests")), [verbose])
-        end,
-        ["binary_search"]).
